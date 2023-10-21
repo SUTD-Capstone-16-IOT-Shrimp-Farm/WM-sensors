c5b6e65a418d4d0e55982285fa8d77ec3745c04f
ssjh23 <seansoojh23@gmail.com>
2023-10-21 23:04:23 +0800

    feat: Button press test to ensure C6 and esp-idf API functions

--- main/main.c
+++ main/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
+    gpio_set_direction(GPIO_NUM_18, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(GPIO_NUM_18, GPIO_PULLUP_ONLY);
 
+    // Print Hello World when button is pressed
+    while (1) {
+        if (gpio_get_level(GPIO_NUM_18) == 1) {
+            printf("Hello World!\n");
+        }
+        vTaskDelay(10);
+    }
 }
